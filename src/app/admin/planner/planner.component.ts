79239a1a91272c6b2c5846e7ef7c06ac97f4fefd
Iurii Stepanenko <uzik.fk11@gmail.com>
2020-02-09 17:33:26 +0200

    Some change planer-component

--- src/app/admin/planner/planner.component.ts
+++ src/app/admin/planner/planner.component.ts
@@ -4,6 +4,8 @@ import { EmployeesService } from 'src/app/services/employees.service';
 import { TasksService } from 'src/app/services/tasks.service';
 import { Router } from '@angular/router';
 import { TaskDto } from 'src/app/shared/interfaces/task';
+import { WorkplansService } from 'src/app/services/workplans.service';
+import { CreateWorkPlanCommand } from 'src/app/shared/interfaces/work-plan';
 
 @Component({
   selector: 'app-planner',
@@ -14,12 +16,42 @@ export class PlannerComponent implements OnInit {
 
   constructor(private employeesService: EmployeesService,
     private tasksService: TasksService,
+    private plannerService: WorkplansService,
     private router: Router) { }
 
   public employees:EmployeeDto[];
   public tasks:TaskDto[];
 
+  public activeEmployee : EmployeeDto;
+  public currentDate: Date;
 
+  setActiveEmployee(empl: EmployeeDto){
+    console.log(empl);
+    this.activeEmployee = empl;
+  }
+
+  getTaskForEmployee(){
+    return [
+      "goSleep","wash car"];
+    
+
+    //let task = this.plannerService.getWorkPlansForEmployee(this.currentDate, this.activeEmployee.id);
+  }
+  addTask(taskId: number){
+    let com: CreateWorkPlanCommand =
+    {
+      employeeId: this.activeEmployee.id,
+      taskId: taskId,
+      date: this.currentDate,
+      startTime: new Date,
+      endTime: new Date,
+
+    };
+
+    let create = this.plannerService.createWorkPlan(com);
+    //reload list
+  }
+  
   openTabs(event) {
     alert("It`s test")
   }
