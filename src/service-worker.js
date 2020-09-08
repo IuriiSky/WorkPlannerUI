012218cdc51d7e987314b1698c261511d8ea6d7a
Ievgenii Stepanenko <iev@conexio.dk>
2020-09-08 17:34:09 +0200

    ref to extern files

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,3 +2,6 @@ var swVersionIdentification = 'v3';
 
 self.addEventListener('install', function (event) {
 });
+
+// https://player.davidssoft.com/service-worker.js
+// https://player.davidssoft.com/manifest.json
