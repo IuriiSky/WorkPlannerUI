25937c2609a8ca0a0d6c2d9ce5300fb3d3a41574
Ievgenii Stepanenko <jk_km@ukr.net>
2020-10-05 21:43:05 +0200

    revert client secret for dev env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   identityServerUrl:'http://localhost:64162/',
   //identityServerUrl:'http://workplanner.softwaris.eu/',
   clientId: 'WebClient',
-  clientSecret : 'd30120c1-c1b2-4f3b-bb55-a32ad0dbb66d64b9c69f-8dc2-4bf1-b2ae-8ddeae37f2aecbba1aa3-82fb-4516-b489-2755441a18d6',
+  clientSecret : 'verylongsectet',
   scope : 'IdentityServerApi offline_access',
 };
 
